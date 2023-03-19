8a566fbf99a849d1494b99747b0e059a1db3d8c0
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2023-03-19 11:11:53 +0530

    Doc string

--- singleton-method.py
+++ singleton-method.py
@@ -1,1 +1,8 @@
 
+''''
+Singleton: A singleton is a creational pattern that restricts the instantiation of a class to one object, 
+ensuring that only one instance of the class exists. For example,
+consider a logger class that is used to write logs to a file.
+'''
+
+
