556d860a6623bd18fbfe78036e1558a2a60e93ec
Jitendra Meena <64771949+jitendra-meena@users.noreply.github.com>
2023-03-19 11:58:22 +0530

    Singlton update

--- singleton-method.py
+++ singleton-method.py
@@ -6,3 +6,32 @@ consider a logger class that is used to write logs to a file.
 '''
 
 
+class Singleton:
+    __instance = None
+    
+    @staticmethod
+    def get_instance():
+        if Singleton.__instance == None:
+            Singleton()
+        return Singleton.__instance
+    
+    def __init__(self):
+        if Singleton.__instance != None:
+            raise Exception("This class is a singleton")
+        else:
+            Singleton.__instance = self
+            
+s1 = Singleton.get_instance()
+s2 = Singleton.get_instance()
+
+print(s1)
+print(s2)
+
+# Output:
+# <__main__.Singleton object at 0x000001>
+# <__main__.Singleton object at 0x000001>
+
+print(s1 == s2)
+
+# Output:
+# True
